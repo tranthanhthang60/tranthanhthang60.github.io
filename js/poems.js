ebbb8de3d72c800fe60f677ec6d0e73ac8b08ccc
Quynh Tran <nhim0406@Quynhs-MacBook-Pro.local>
2016-08-07 17:18:35 +0100

    added poems

--- js/poems.js
+++ js/poems.js
@@ -1,40 +1,128 @@
-var poem1 = {
+var poem3 = {
 		"title" : "KỶ NIỆM",
 		"text" : "<p>Kho tàng kỷ niệm thật mênh mông<br>Những vật vô tri đầy ý tứ<br>Những chuyện vui buồn trong quá khứ<br>Kiểu ngồi, dáng đứng, ánh mắt trông...<br>Kỷ niệm chẳng hề giống dòng sông<br>Với bến đợi vẽ tô hào nhoáng<br>Kỷ niệm như mảng đời ngắt quãng<br>Chợt ào về giây phút ngẩn ngơ !<br><br>Kỷ niệm đôi lúc đẹp như mơ<br>Có khi dằng dai như chủ nợ<br>Từng thời chéo chồng trong nỗi nhớ<br>Như họa đồ những chốn kinh qua !<br><br>Kỷ niệm chẳng hề giống sân ga<br>Mỏi mòn đón đưa người qua lại<br>Kỷ niệm giống như tàu không lái<br>Chẳng biết bao giờ hết lang thang !<br><br>Kỷ niệm xẻ chia niềm xốn xang<br>Là lời ru khi đời khốn khó<br>Là gói hành trang ảo huyền nho nhỏ<br>Là những gì sống chết cũng gắng mang !<br></p>",
-		"img" : "img/2.png"		
+		"img" : "img/12.png"		
 };
 
 var poem2 = {
-		"title" : "LÀM ÔNG CHẲNG DỄ",
-		"text" : "dksjdlksajdlska",
-		"img" : "img/3.png"		
+		"title" : "BƯỚC CHÂN",
+		"text" : "<p>Bước chân Bà chậm dần thôi bươn chãi<br>Đầu ngón chân hờ bám bậc thềm<br>Gót chân nhỏ nhạt mờ thời con gái<br>À ơi ru cháu thật dịu êm !<br><br>Bước chân Ông hết mòn sỏi đá<br>Lưu tiếng vang một thủa mê say<br>Giờ quanh quẩn vườn cây ao cá<br>Vết chai chân bợt mỏng mỗi ngày !<br><br>Bước chân Con ồn ào tất tả<br>Cuộc đời này rồi sẽ ra sao<br>Chân ngập ngừng tìm miền đất lạ<br>Ước mơ nhiều đâu thỏa nỗi khát khao !<br><br>Cháu tập đi, đôi chân trần quả quyết<br>Cứ ngã ngồi lại đứng dậy bằng tay...<br>Mai này đôi chân nhiều hiểu biết<br>Liệu có còn thẳng bước giống hôm nay !<br></p>",
+		"img" : "img/16.png"		
 };
 
-var poem3 = {
+var poem1 = {
 		"title" : "LÀM ÔNG CHẲNG DỄ",
-		"text" : "sjkdlajskdl dskajldsa <br> dsjkaldjska <br>dsakdljsakdksla",
+		"text" : "<p>Làm ông chẳng dễ tý nào<br>Cháu còn quá bé chưa chào được ông<br>Chóp cha chóp chép giấc nồng<br>Bà lo cháu dậy đuổi ông ra ngoài !<br><br>Làm ông có lúc mệt nhoài<br>Làm trâu làm ngựa bò toài khắp nơi<br>Làm ông phải biết quên thời<br>Giả ngây giả ngố để chơi thực lòng !<br><br>Làm ông lưng đỡ bị còng<br>Mắt kia đỡ mỏi, da trông hồng hào...<br>Làm ông chẳng dễ tý nào<br>Tuổi già trăn trở bước vào tuổi thơ !<br></p>",
+		"img" : "img/2.png"		
+};
+
+var poem4 = {
+		"title" : "CHUỒN CHUỒN NGÔ",
+		"text" : "<p>Vẳng nghe một khúc nhạc buồn<br>Tự dưng bỗng nhớ chú chuồn chuồn ngô<br>Đuôi dài cánh rộng mắt thô<br>Chập chờn nhấp nước trên hồ trong veo.<br><br>Tuổi thơ sơ tán xã nghèo<br>Vớt bèo chủ nhật nuôi heo chủ nhà<br>Cánh bèo kết lại thành phà<br>Để chuồn ngô đậu như là trực thăng.<br><br>Sinh vào thời buổi khó khăn<br>Hồn nhiên và chẳng băn khoăn tý nào<br>Chuồn ngô bay thấp bay cao<br>Điều mưa khiển nắng, ca dao gọi chuồn...<br><br>Niềm vui sinh tự nỗi buồn<br>Cảm như nặng những cánh chuồn trên vai !<br></p>",
+		"img" : "img/10.png"		
+};
+
+var poem5 = {
+		"title" : "VÉ VỀ TUỔI THƠ",
+		"text" : "<p>Ta từng mong có một tuổi thơ<br>Đầy tình thương với nụ cười tươi tắn<br>Sáng học bi bô, chiều bắt chuồn chuồn trong nắng<br>Nhảy lò cò, góc phố vắng đá banh !<br><br>Ta đã ước có một tuổi thơ<br>Bé don don không phải dọn hầm chữ A trong rét mướt<br>Không phải tự trồng rau, nuôi gà nộp Quỹ Cứu Nước<br>Bao lúc đói lòng chỉ mong gặp Bụt trong mơ !<br><br>Có bao người mong Chiếc vé về Tuổi thơ<br>Để tạm trốn đời thường đầy tất bật<br>Để tìm lại những gì trong sáng nhất<br>Mà tự mình đánh mất lúc lớn lên !<br><br>Đời dìm người bao cơn sóng không tên<br>Dẫu nhiều đèn khoảng giao thoa vẫn tối<br>Lòng tôi sợ cái Tuổi thơ dữ dội<br>Không muốn về, sao chữ NHỚ át chữ QUÊN !<br></p>",
+		"img" : "img/5.png"		
+};
+
+var poem6 = {
+		"title" : "KÝ ỨC",
+		"text" : "<p>Cuộc đời thường đo bởi tháng năm<br>Có thể kéo dài bằng tiếng tăm<br>Với tôi đời đo bằng ký ức<br>Khi dài khi ngắn, lúc xa xăm !<br><br>Ký ức ban đầu rất đơn sơ<br>Pha chút huyền thoại của tuổi thơ<br>Thả diều, đổ dế, chơi đuổi bắt...<br>Hồn nhiên vui giữa những giấc mơ !<br><br>Ký ức bộn bề lên rất mau<br>Từ thủa mười tám bước lên tàu<br>Ký ức dường như lọt vòng xoáy<br>Khi chạm vào hai chữ Yêu nhau !<br><br>Ký ức nào chẳng chứa bão giông<br>Lỗi lầm, khát vọng, vai gánh gồng...<br>Bao người thân hữu đâu rồi nhỉ<br>Tiếc nuối hòa cùng những ngóng trông !<br><br>Tìm về ký ức giống ngược sông<br>Ào ào hiện tại nỗi bao đồng<br>Dẫu biết ký ức là nguồn sống<br>Liệu mình đủ sức để Xuyên Không !<br></p>",
+		"img" : "img/15.png"		
+};
+
+var poem7 = {
+		"title" : "THỜI TRAI TRẺ",
+		"text" : "<p>Thời trai trẻ mình đầy cơ bắp<br>Nhảy cầu thang ba bốn bậc như không<br>Thời trai trẻ hai chân đi thẳng tắp<br>Bao ước mơ phía trước đợi ngóng trông !<br><br>Thời trai trẻ ít phải lo thất nghiệp<br>Chữ trong đầu chẳng khó dựng tiếng thơm<br>Thời trai trẻ chờ mong ngày đưa thiệp<br>Có vợ rồi, nhà như thể có cơm !<br><br>Thời trai trẻ biết yêu chưa biết sống<br>Ngày nối ngày mấy lúc trống vắng đâu<br>Thời trai trẻ ào ào như biển động<br>Chẳng kịp ngồi tĩnh lặng ngẫm nông sâu !<br><br>Thời trai trẻ kiểu gì cũng đẹp nhất<br>Dẫu bấp bênh ngơ ngác những mắt nai...<br>Có lẽ mọi Ông già trên trái đất<br>Đều tự hào đã sống một thời trai !<br></p>",
 		"img" : "img/1.png"		
 };
 
-var poems = [poem1, poem2];
+var poem8 = {
+		"title" : "CHIỀU MÙA HẠ",
+		"text" : "<p>Chiều mùa Hạ sao trời không chịu tối<br>Lứa đôi hò hẹn đếm từng giây<br>Trăng lấp ló giục mặt trời lặn vội<br>Dưới nắng chiều, tình chẳng thể ngất ngây !<br><br>Chiều mùa Hạ như không ai cần cả<br>Quán ven đường lác đác khách vãng lai<br>Chiều mùa Hạ bóng cây che vất vả<br>Mệt lắm rồi oi ả tiếng ve dai !<br><br>Liệu có thể không cần chiều Hạ nữa<br>Bớt tiếng ve, đỡ giọt mồ hôi lăn<br>Trời đang cháy sẽ vụt tàn rụi lửa<br>Bóng đêm dài đâu ngắn lại nếp nhăn !<br><br>Chiều mùa Hạ cỏ cây đòi uống nước<br>Nắng xiên xiên rạch xướt những cánh hoa<br>Chiều mùa Hạ rối chân bao nhịp bước<br>Bồn chồn, thêm nỗi khát trong ta !<br><br>Chiều mùa Hạ ẩn giấu những phong ba<br>Giục giã người đời về mái ấm...<br>Chiều mùa Hạ đôi khi như trái cấm<br>Nửa hững hờ nửa bỏng cháy diết da !<br></p>",
+		"img" : "img/14.png"		
+};
+
+var poem9 = {
+		"title" : "ĐIỀU KHÓ NÓI",
+		"text" : "<p>Điều khó nói nén dồn như sóng nước<br>Vỗ vào tâm khảm lại dội về<br>Khó nói đâu phải không nói được<br>Sợ nói ra rồi nhỡ bị chê !<br><br>Mấy mươi năm trước trai mới lớn<br>Mê câu: Khó nói lắm anh ơi !<br>Cái thủa ấy, cầm tay đã nghẹn lời<br>Điều khó nói ẩn mình bên phố nhỏ !<br><br>Tháng năm trôi, đất bao lần thay cỏ<br>Nắng chẳng kiêng ai, mặt rám dạn dày<br>Uống rượu đời đang ở cuối cuộc say<br>Trơn tru liệu còn gì khó nói !<br><br>Đời như hoa, đến mùa lại tươi rói<br>Người như sao, lúc sáng lúc rơi<br>Tình như lửa khi bùng khi le lói<br>Cháy bằng gì khó nói lắm em ơi !<br></p>",
+		"img" : "img/11.png"		
+};
+
+var poem10 = {
+		"title" : "TÁT NƯỚC BIỂN MƠ",
+		"text" : "<p>Khổ đau lệ tự tuôn trào<br>Hòa cùng sông suối chảy vào biển Đông !<br><br>Những khi thuận vợ thuận chồng<br>Hò nhau tát biển bềnh bồng gàu đưa<br>Để rồi sớm nắng chiều mưa<br>Vòng đời lặp lại chuyện xưa Dã Tràng...<br><br>Biển là cớ để mơ màng<br>Nếu mà Biển cạn Khôn Càn ra sao ?<br>Với Nàng Thi Sĩ năm nao<br>Biển là cô gái ngọt ngào sắc xuân<br>Biển đưa Thuyền vượt gian truân<br>Khiến lòng Thuyền rộn bâng khuâng hẹn hò...<br><br>Ngẫm mình phận bé con đò<br>Đành ngồi mơ Biển như cò mơ trăng !<br>Người đời cưỡi sóng tung tăng<br>Nhớ chăng đáy biển nứt hằn từ lâu<br>Muôn dòng lệ đổ biển sâu<br>Biển Mơ ta hiểu do đâu mặn mòi !<br></p>",
+		"img" : "img/4.png"		
+};
+
+var poem11 = {
+		"title" : "GIẬN HỜN",
+		"text" : "<p>Em giận hờn, dáng dấp kiêu sa<br>Môi dẩu thành nụ hoa chúm chím<br>Đôi mắt đen giờ pha màu tím<br>Tóc lòa xòa che chắn bờ vai !<br><br>Giận anh, bông hồng nhỏ xù gai<br>Như đẩy chân anh về chốn cũ<br>Về thủa ban đầu đầy quyến rũ<br>Ơi giận hờn vị ngọt lứa đôi !<br><br>Sẽ ra sao cô bé của tôi<br>Nếu mất đi cái hờn cái dỗi ?<br>Chắc sẽ như cây không gió thổi<br>Lá lặng im chẳng hát rì rào !<br><br>Với giận hờn, vũ khí trời trao<br>Em bảo vệ điều mình mong muốn<br>Cái nguýt dài làm anh luống cuống<br>Lúc đang yêu ai chẳng vụng về...<br><br>Anh uống hờn giận đến thỏa thê<br>Anh yêu ngọt ngào pha vị đắng<br>Phía sau giận hờn là khoảng lặng<br>Phút làm lành đắng ngọt đê mê !<br></p>",
+		"img" : "img/17.jpg"		
+};
+
+var poem12 = {
+		"title" : "CHỈ ĐÁ TRONG MƠ",
+		"text" : "<p>Bóng đá tuổi thơ trong chiến tranh<br>Vỉa hè chân đất đá thả phanh<br>Giẻ rách cuộn tròn hình quả bóng<br>Ngón chân kẹp giẻ phá khung thành !<br><br>Nhớ quả bóng hỏng nhặt ven sông<br>Đem về tạo dáng mũ phi công<br>Trẻ con lối xóm tặng khoai sắn<br>Để xin đội mũ thật là đông !<br><br>Ta học đá bóng lúc hai mươi<br>Đất Hung rung chuyển những trận cười<br>May mà có một hoa chân nhỏ<br>Nên được mang chân đá sân người !<br><br>Cái thời mê đá đến ngẩn ngơ<br>Cớ sao chẳng kéo tới bây giờ<br>Hấp háy mục kỉnh xem World Cup<br>Để rồi tung cẳng đá trong mơ !<br></p>",
+		"img" : "img/13.png"		
+};
+
+var poem13 = {
+		"title" : "VƯỢT ĐÈO",
+		"text" : "<p>Đèo cao, cao tận lưng trời<br>Đèo ôm lấy núi như đời ôm nhau<br>Đời ôm lúc sướng lúc đau<br>Ngắn dài như dốc nối nhau trên đèo.<br><br>Trầm ngâm lữ khách gắng trèo<br>Ngâm mình trong gió, ngoằn ngoèo tâm tư:<br>Đời thường vùn vụt thực hư<br>Ở lưng chừng núi mới dư thời giờ<br>Ngắm đời khi đã mệt phờ<br>Xa xa dưới núi lờ mờ bóng ai ?<br>Mắt hoa mới rõ tương lai<br>Ù tai mới hiểu đường dài bao nhiêu !<br><br>Thoảng qua giây phút đáng yêu<br>Lại về nguyên dạng như nhiều sinh linh.<br>Vượt Đèo, đèo chỉ lặng thinh<br>Vượt Người, như thể vô tình thành Gai !!!<br></p>",
+		"img" : "img/9.png"		
+};
+
+var poem14 = {
+		"title" : "BẠN HỎI ?",
+		"text" : "<p>Bạn hỏi vì sao có chữ Tình<br>Vì sao ai đó chịu hy sinh<br>Những khi ta lỡ bị thua thiệt<br>Các câu hỏi trên lại rập rình !<br><br>Bạn hỏi vì sao có ước mơ<br>Vì sao ai đó phất được cờ<br>Những khi thuyền đảo, buồm rũ cánh<br>Chồn tay chèo lái mặt ngẩn ngơ !<br><br>Bạn hỏi vì sao bị bơ vơ<br>Vì sao ai đó cứ hững hờ<br>Thân làm người lớn là như vậy<br>Giá mà bé mãi với tuổi thơ !<br><br>Bạn hỏi cái gì đẹp rất lâu<br>Vì sao ai cũng nhớ Tình Đầu<br>Đời thường bếp núc sôi trào nắp<br>Chập chờn hình bóng lúc đêm thâu !<br><br>Bao nhiêu câu hỏi nối tiếp nhau<br>Như đời xâu chuỗi những nỗi đau...<br>Khi nào sẽ tới cuộc Tình Cuối<br>Câu trả lời tìm ở Kiếp sau !<br></p>",
+		"img" : "img/7.png"		
+};
+
+var poem15 = {
+		"title" : "QUÊ XA",
+		"text" : "<p>Ngày đi con bé mẹ bồng<br>Con giờ khôn lớn bén thông tay nghề...<br>Xứ người kiếm sống bộn bề<br>Có ai câu nệ mà chê bán hàng<br>Nắng mưa bụi phủ đầu chàng<br>Nhìn băng tuyết cóng tay nàng mà thương !<br>Dòng đời bơi bạc thái dương<br>Bóng ai đổ xuống lót đường hậu sinh !<br><br>Xa quê là chuyện thường tình<br>Mấy ai ở mãi nơi mình sinh ra<br>Kiếp người trôi nổi gần xa<br>Giống như hạt cỏ gió tha đâu về !<br><br>Sống là ôm nặng chữ Quê<br>Khói nhang dẫn dắt ai về thăm ai !<br></p>",
+		"img" : "img/3.png"		
+};
+
+var poems = [poem1, poem2, poem3, poem4, poem5, poem6, poem7, poem8, poem9, poem10, poem11, poem12, poem13, poem14, poem15];
+
+function updatePage(j) {
+	return function() {
+		poem = poems[j];
+		$("h1").replaceWith("<h1>" + poem.title + "</h1>");
+		$(".photo-container").css("background-image", "url(" + poem.img + ")");
+		$(".poem").html(poem.text);
+		$('.open').removeClass('open');
+	}
+}
 
 $(document).ready(function(){
-    $("#btn1").click(function(){
-        $("h1").replaceWith("<h1>" + poem1.title + "</h1>");
-        $(".photo-container").css("background-image", "url(" + poem1.img + ")");
-        $(".poem").html(poem1.text);
-        $('.open').removeClass('open');
-    });
-    $("#btn2").click(function(){
-        $("h1").replaceWith("<h1>" + poem2.title + "</h1>");
-        $(".photo-container").css("background-image", "url(" + poem2.img + ")");
-        $(".poem").html(poem2.text);
-        $('.open').removeClass('open');
-    });
-     $("#btn3").click(function(){
-        $("h1").replaceWith("<h1>" + poem3.title + "</h1>");
-        $(".photo-container").css("background-image", "url(" + poem3.img + ")");
-        $(".poem").html(poem3.text);
-        $('.open').removeClass('open');
-    });
+	for (var i=0; i<poems.length; i++) {
+		buttonnum = i + 1;
+		buttonname = "#btn" + buttonnum;
+		$(buttonname).click(updatePage(i));
+	} 
+
+    // $("#btn1").click(function(){
+    //     $("h1").replaceWith("<h1>" + poem1.title + "</h1>");
+    //     $(".photo-container").css("background-image", "url(" + poem1.img + ")");
+    //     $(".poem").html(poem1.text);
+    //     $('.open').removeClass('open');
+    // });
+    // $("#btn2").click(function(){
+    //     $("h1").replaceWith("<h1>" + poem2.title + "</h1>");
+    //     $(".photo-container").css("background-image", "url(" + poem2.img + ")");
+    //     $(".poem").html(poem2.text);
+    //     $('.open').removeClass('open');
+    // });
+    //  $("#btn3").click(function(){
+    //     $("h1").replaceWith("<h1>" + poem3.title + "</h1>");
+    //     $(".photo-container").css("background-image", "url(" + poem3.img + ")");
+    //     $(".poem").html(poem3.text);
+    //     $('.open').removeClass('open');
+    // });
 });
